aa743f4522f8f0b6f7811f00a4fb1b9bfd2b963c
Viktor Maar <viktor.maar@exaktasoftware.se>
2019-11-26 15:43:14 +0100

    Apply improvements to accounts

---
+++ src/Providers/Accounts/Resource.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Wetcat\Fortie\Providers\Accounts;
+
+use Wetcat\Fortie\AbstractFortieResource;
+
+class Resource extends AbstractFortieResource
+{
+    protected static $wrapper = 'Accounts';
+}
