4f3f5295c7919d1343a1f2e65df3af0ba2297003
Viktor Maar <viktor.maar@exaktasoftware.se>
2019-11-26 14:38:22 +0100

    Remove unused classes

--- src/Contracts/Customers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Wetcat\Fortie\Contracts;
-
-class Customers extends AbstractResponse
-{
-}
--- src/Contracts/Units.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Wetcat\Fortie\Contracts;
-
-class Units extends AbstractResponse
-{
-}
