49837a7e268a6d642395ee133643a3d910e48c83
Viktor Maar <viktor.maar@exaktasoftware.se>
2019-11-27 15:03:29 +0100

    Apply improvements to article file connections

--- src/Providers/ArticleFileConnections/Provider.php
+++ src/Providers/ArticleFileConnections/Provider.php
@@ -18,10 +18,24 @@
 
 */
 
-use Wetcat\Fortie\Providers\ProviderBase;
 use Wetcat\Fortie\FortieRequest;
+use Wetcat\Fortie\Providers\ProviderBase;
+use Wetcat\Fortie\Traits\CountTrait;
+use Wetcat\Fortie\Traits\CreateTrait;
+use Wetcat\Fortie\Traits\DeleteTrait;
+use Wetcat\Fortie\Traits\FetchTrait;
+use Wetcat\Fortie\Traits\FindTrait;
 
 class Provider extends ProviderBase {
+
+  use CountTrait,
+      CreateTrait,
+      DeleteTrait,
+      FetchTrait,
+      FindTrait;
+
+  protected $wrapper = 'ArticleFileConnection';
+  protected $wrapperGroup = 'ArticleFileConnections';
 
   protected $attributes = [
     'Url',
@@ -46,100 +60,16 @@ class Provider extends ProviderBase {
 
 
   /**
+   * The possible values for filtering.
+   *
+   * @var array
+   */
+  protected $available_filters = [
+  ];
+
+
+  /**
    * Override the REST path
    */
   protected $basePath = 'articlefileconnections';
-
-
-  /**
-   * Retrieves a list of article file connections or a single article
-   * file connection.
-   *
-   * @return array
-   */
-  public function all ($page = null)
-  {
-    $req = new FortieRequest();
-    $req->method('GET');
-    $req->path($this->basePath);
-
-    if (!is_null($page)) {  
-      $req->param('page', $page);
-    }
-
-    return $this->send($req->build());
-  }
-
-
-  /**
-   * Retrieves the details of an account. You need to supply the unique 
-   * account number that was returned when the account was created or 
-   * retrieved from the list of accounts.
-   *
-   * @param $fileId
-   * @return array
-   */
-  public function find ($fileId)
-  {
-    $req = new FortieRequest();
-    $req->method('GET');
-    $req->path($this->basePath)->path($fileId);
-
-    return $this->send($req->build());
-  }
-
-
-  /**
-   * Creates a new absence transaction for an employee.
-   *
-   * @param array   $data
-   * @return array
-   */
-  public function create (array $data)
-  {
-    $req = new FortieRequest();
-    $req->method('POST');
-    $req->path($this->basePath);
-    $req->wrapper('ArticleFileConnection');
-    $req->data($data);
-    $req->setRequired($this->required_create);
-
-    return $this->send($req->build());
-  }
-
-
-  /**
-   * Updates an absence transaction.
-   *
-   * @param array   $data
-   * @return array
-   */
-  public function update ($fileId, array $data)
-  {
-    $req = new FortieRequest();
-    $req->method('PUT');
-    $req->path($this->basePath)->path($fileId);
-    $req->wrapper('ArticleFileConnection');
-    $req->setRequired($this->required_update);
-    $req->data($data);
-
-    return $this->send($req->build());
-  }
-
-
-  /**
-   * Deletes the article permanently.
-   *
-   * You need to supply the unique article number that was returned when the 
-   * article was created or retrieved from the list of articles.
-   */
-  public function delete ($fileId)
-  {
-    $req = new FortieRequest();
-    $req->method('DELETE');
-    $req->path($this->basePath)->path($fileId);
-
-    return $this->send($req->build());
-  }
-
 }
