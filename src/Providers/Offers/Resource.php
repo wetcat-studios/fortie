ce79543d2038b18e7b58f9dad2568817b874c7bd
Viktor Maar <viktor.maar@exaktasoftware.se>
2019-11-27 15:16:57 +0100

    Add improvements to offers

---
+++ src/Providers/Offers/Resource.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Wetcat\Fortie\Providers\Offers;
+
+use Wetcat\Fortie\AbstractFortieResource;
+
+class Resource extends AbstractFortieResource
+{
+    protected $wrapper = 'Offers';
+}
