43be83f0c87f7db48020bc43a2117bc54b0bbd5e
Viktor Maar <viktor.maar@exaktasoftware.se>
2019-11-27 15:10:07 +0100

    Add improvements to cost centers

---
+++ src/Providers/CostCenters/Resource.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Wetcat\Fortie\Providers\CostCenters;
+
+use Wetcat\Fortie\AbstractFortieResource;
+
+class Resource extends AbstractFortieResource
+{
+    protected $wrapper = 'CostCenters';
+}
